ad6e90945a82fc3aead24a06d01f00b107ad0645
Jeremy Pry <jeremy.pry@gmail.com>
2023-08-18 18:29:53 -0400

    Remove experimental feature flag, and enable by default

--- plugins/woocommerce/src/Internal/Features/FeaturesController.php
+++ plugins/woocommerce/src/Internal/Features/FeaturesController.php
@@ -141,10 +141,11 @@ class FeaturesController {
 				'disable_ui'         => false,
 			),
 			'order_source_attribution' => array(
-				'name'            => __( 'Order Source Attribution (Beta)', 'woocommerce' ),
-				'description'     => __( 'Enable this feature to track and credit channels and campaigns that contribute to orders on your site.', 'woocommerce' ),
-				'is_experimental' => true,
-				'disable_ui'      => false,
+				'name'               => __( 'Order Source Attribution (Beta)', 'woocommerce' ),
+				'description'        => __( 'Enable this feature to track and credit channels and campaigns that contribute to orders on your site.', 'woocommerce' ),
+				'is_experimental'    => false,
+				'disable_ui'         => false,
+				'enabled_by_default' => true,
 			),
 		);
 
